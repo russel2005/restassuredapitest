e3f6ae455593fb1231262155bca8aa1eafd07e8e
Touhidul Islam <russel2005@gmail.com>
2020-03-10 15:50:18 -0400

    Create GetCallBDD.java

---
+++ RestAssuredAPITesting/src/test/java/testscript/GetCallBDD.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class GetCallBDD {
+	
+	@Test
+	public void test_NumberOfCircuitesFor2017_Season(){
+		given().
+		when().
+			get("http://ergast.com/api/f1/2017/circuits.json").
+		then().
+			assertThat().
+			statusCode(200).
+			and().
+			body("MRData.CircuitTable.Circuits.circuitId", hasSize(20)).
+			and().
+			header("contest-length", equalTo("4551"));
+	}
+}
